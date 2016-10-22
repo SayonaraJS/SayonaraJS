b90ffe6e30796299dd2d776f8c5f8d5e643c5475
Aaron Turner <aaron@aaronthedev.com>
2016-10-22 04:57:56 +0000

    Generated the admin application

---
+++ SayonaraAdmin/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sayonaraAdminApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sayonaraAdminApp
+ */
+angular.module('sayonaraAdminApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ SayonaraAdmin/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sayonaraAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sayonaraAdminApp
+ */
+angular.module('sayonaraAdminApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
