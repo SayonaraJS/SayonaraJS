158509181cce596bbf35d74946d3c66a4cc979da
Aaron Turner <aaron@aaronthedev.com>
2017-04-13 12:02:37 -0700

    Finished the custom fields directive...for reals

--- SayonaraAdmin/app/scripts/directives/sayonaracustomfield.js
+++ SayonaraAdmin/app/scripts/directives/sayonaracustomfield.js
@@ -46,6 +46,20 @@ angular.module('sayonaraAdminApp')
 
         // Function to create or delete the fields attribute if empty
         $scope.createDeleteFieldIfEmpty = function(customFieldTypeId, event) {
+
+          //Only accept character and number keys
+          // Inner If check for backspace
+          if(event.key &&
+            event.keyCode < 48 ||
+            (event.keyCode > 90 &&
+            event.keyCode < 96) ||
+            event.keyCode > 105) {
+            // Return if the key is not backspace
+            if(event.keyCode != 8) {
+              return;
+            }
+          }
+
           // Get the custom field if it exists
           var customField = $scope.getCustomFieldByTypeId(customFieldTypeId);
           if(!customField && event.keyCode != 8) {
@@ -53,9 +67,15 @@ angular.module('sayonaraAdminApp')
               customFieldType: customFieldTypeId,
               fields: []
             });
+          } else if(customField.fields &&
+            customField.fields.length > 1 &&
+            customField.fields[customField.fields.length - 1].length <= 1 &&
+            event.keyCode == 8) {
+            // Splice from the array
+            customField.fields.pop();
           } else if (customField.fields &&
             customField.fields.length == 1 &&
-            !customField.fields[0] &&
+            customField.fields[0].length <= 1 &&
             event.keyCode == 8) {
             // Remove the custom field
             $scope.customFields.some(function(customField, index) {
